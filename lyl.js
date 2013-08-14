31d155a7266b01da7a04a7b0027f164d8eed9ea6
limyra <root@liyongliangmatoMacBook-Pro.local>
2013-08-14 17:53:21 +0800

    add some content

--- lyl.js
+++ lyl.js
@@ -1,6 +1,12 @@
-function () {
+function() {
 	alert('SOSO!');
 	console.log('very good!');
 	console.log('How to get it!');
-    console.log('goodddd!');
+	console.log('goodddd!');
+}
+
+
+function main() {
+	// body...
+	alert(123);
 }
